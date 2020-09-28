d6a13d26eec270891219a929479c668278d81303
hyenni2-2 <hi.choi1031@gmail.com>
2020-09-28 17:43:30 +0900

    200928 Class

--- java_project/FirstJava/src/first/FirstJava.java
+++ java_project/FirstJava/src/first/FirstJava.java
@@ -1,10 +1,17 @@
 package first;
 
+// first.FirstJava
+
 public class FirstJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		System.out.println("hello~ JAVA");
+		
+		//sysout + ctl + space
+		
+		
 	}
 
 }
