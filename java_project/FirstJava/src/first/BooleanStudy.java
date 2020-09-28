b9b209167e7bd634f4e7c4bee74e9b05b29dd744
hyenni2-2 <hi.choi1031@gmail.com>
2020-09-28 19:11:38 +0900

    boolean study

--- java_project/FirstJava/src/first/BooleanStudy.java
+++ java_project/FirstJava/src/first/BooleanStudy.java
@@ -7,6 +7,7 @@ public class BooleanStudy {
 	public static void main(String[] args) {
 		// 두 개의 정수를 입력받아 큰 수를 작은 수로 나눈 몫과 나머지 출력
 		
+		/*
 		Scanner sc = new Scanner(System.in);
 		
 		int Num1;
@@ -26,11 +27,59 @@ public class BooleanStudy {
 			System.out.println("몫은 " + Num2/Num1);
 			System.out.println("나머지는" + Num2%Num1);
 
+
 	}
 
+*/		
+		
+		// 메트로폴리스
+		
+		boolean isCapital;
+		int jongro;
+		int riches;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("19세 이상입니까?(19세 이상: 1 19세 미만 : 0)");
+		isCapital = (sc.nextInt() == 1) ? true : false;
+	    
+		
+	    if(isCapital=true) {
+	    	System.out.println("종로구에 거주합니까?(yes:1 no : 0)");
+	    	isCapital = (sc.nextInt() == 1) ? true : false;
+	    		if(isCapital=true){
+	    	    	System.out.println("선거권이 있습니다.");
+	    		}
+	    		else {
+	    	    	System.out.println("선거권이 없습니다.");
+	    		} 
+	    }
+	    /*
+	    else {
+	    	System.out.println("선거권이 없습니다.");
+	    }
+		*/
 		
 		
 		
-		//cm로 표현된 키 입력하여 피트와 인치로 변환. 
+		
+		
+		
+		
+		
+		
+		
+		//서울 종로구 지방선거권 유무 확인 프로그램 만들기
+		
+		int a;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
