432608110184695e92be83b0a9c73fa2049877bc
HyeinChoi <hi.choi1031@gmail.com>
2020-10-06 10:47:08 +0900

    201006 add

---
+++ java_project/FirstJava/src/first/ExamOper1.java
@@ -0,0 +1,26 @@
+package first;
+
+public class ExamOper1 {
+
+	public static void main(String[] args) {
+		// 문제 1
+		int num1=10;
+		int num2=20;
+		int num3=30;
+		
+		System.out.println(num1 + ", " + num2+", "+num3);
+
+		num1=num2=num3;
+		System.out.println(num1 + ", " + num2+", "+num3);
+	
+		num1=num2=num3=50;
+		System.out.println(num1 + ", " + num2+", "+num3);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ java_project/FirstJava/src/first/ExamVariable.java
@@ -0,0 +1,13 @@
+package first;
+
+public class ExamVariable {
+
+	public static void main(String[] args) {
+		String regNo = "001006 - 2000000";
+		// regNo = 0000000000000L; 이 경우 앞 0 두 개가 생략되므로 String을 쓰는 게 좋음.
+		
+		System.out.println(regNo);
+
+	}
+
+}
