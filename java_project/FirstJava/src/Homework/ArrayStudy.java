e7a517807d55c7fecfde0bae41560730174961d3
HyeinChoi <hi.choi1031@gmail.com>
2020-10-15 02:08:57 +0900

    201014 HW

--- java_project/FirstJava/src/Homework/ArrayStudy.java
+++ java_project/FirstJava/src/Homework/ArrayStudy.java
@@ -1,32 +1,48 @@
 package Homework;
 
+import java.util.Scanner;
 
 public class ArrayStudy {
 
 	public static void main(String[] args) {
-//		문제 1.
-//		int형 1차원 배열을 매개변수로 전달 받아서 배열에 저장된 최대값,
-//		그리고 최소값을 구해서 반환하는 메소드를 다음의 형태로 각각 정의.
-//		public static int miniValue(int[] arr) { . . . . } // 최소값 반환
-//		public static int maxValue(int[] arr) { . . . . } // 최대값 반환
-//		위의 두 매소드는 인자로 전달되는 배열의 길이에 상관없이 동작하도록 정의.
-//		두 매소드 실행을 확인하기 위한 main 메소드 정의.
-//		int형 배열에 채워질 정수는 프로그램 사용자로부터 입력 받고, 배열의 길이는
-//		임의로 결정
 
-		ArrayStudy value = new ArrayStudy();
-		int[] arr = {10,30,20,1,5};
+
+		Scanner sc = new Scanner(System.in);
+		int[] arr; // 배열 선언
+	    int length; // 배열의 길이 선언 
+		System.out.println("배열의 길이는? ");   // 길이를 먼저 알아야 배열이 몇 개 들어가는지 알 수 있다.
+		length = sc.nextInt();
+		arr = new int[length];  // 길이를 넣어서 길이 = 배열 요소 개수니까. 
+	    for(int i=0; i<arr.length; i++) {
+	    	System.out.println((i+1) +"회차 요소는 : ");
+	    	arr[i] = sc.nextInt(); // 배열 안의 arr[i]값을 참조하고 싶은거니까, arr[i]로 넣어야함. arr로 넣으면 주소값나옴;;
+	    }
+		ArrayStudy value = new ArrayStudy(); // 호출 메서드 선언
 		System.out.println("minNum : " + value.miniValue(arr));
 		System.out.println("maxNum : " + value.maxValue(arr));
 		
-		int[][] arr1 = new int[][]{
-				{2,5,4,1,6},
-				{3,1,2,6,5},	
-		};
-		value.addOneDArr(arr1, 7);
-		System.out.println(arr1);
+        System.out.println("=========================");
 
-	}
+		int[][] arr1 = {
+				{10,30,20,1,5},
+				{1,3,5}
+		};
+        value.addOneDArr(arr1, 4);
+
+        System.out.println("=========================");
+        
+        int[][] arr2 = {
+        		{1,2,3},
+        		{4,5,6},
+        		{7,8,9}
+        };
+        value.shiftArr(arr2);
+        
+        	}
+        	
+        
+        
+	
 		
 	
 	
@@ -52,20 +68,71 @@ public class ArrayStudy {
         return max;
 	}
 	
-	// 문제 2
-	void addOneDArr(int[] arr, int add) {
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = arr[i] + add;
-		}
-	}
+// 문제 2
+//	이 메서드를 기반으로(이 메서들르 호출하는 형태로)
+// int형 2차원 배열에 저장된 값 전부를 증가시키는 메서드를 다음의 형태로 정의하자.
+//	public static void addOneDArr(int[][] arr, int add) { . . . . . }
+//	단 위 메서드는 2차원 배열의 가로, 세로 길이에 상관없이 동작해야 하며, 
+// 위의 메서드가 제대로 동작하는지 확인하기 위한 main 메서드도 함께 정의해야 한다.
 	
-	void addOneDArr(int[][] arr1, int add) {
+//	void addOneDArr(int[] arr, int add) {  // 주어진 메서드
+//		for(int i=0; i<arr.length; i++) {  // 이 메서드를 호출해서 2차원으로.
+//			arr[i] = arr[i]+add;
+//		}
+//	}
+	
+	
+	
+	
+	void addOneDArr(int[][] arr1, int add) {    //2차원 배열 메서드
 		for(int i=0; i<arr1.length; i++) {
 			for(int j=0; j<arr1[i].length; j++) {
 				arr1[i][j] = arr1[i][j] + add;
+				System.out.print(arr1[i][j]+" "); // 배열 정리하기 위해 print와 " " 공백 포함
 			}
+			System.out.println(); // 개행
 		}
-		
 	}
-	
+		
+		
+//		문제 3.
+//		다음의 형태로 표현된 2차원 배열이 존재한다고 가정해 보자.
+//		1 2 3
+//		4 5 6
+//		7 8 9
+//		이러한 형태를 갖는 int형 2차원 배열이 인자로 전달되면, 
+//		다음의 형태로 배열의 구조를 변경시키는 메서드를 정의해 보자.
+//		7 8 9
+//		1 2 3
+//		4 5 6
+//      즉 총 N행으로 이뤄진 2차원 배열이 존재한다면, 
+//      메서드 호출 시, 1행은 2행으로 이동이 이뤄져야한다. 
+        void shiftArr(int[][] arr2) {
+           int[][] temp = new int[arr2.length][arr2[0].length]; // 열은 바뀌지 않음.
+           for(int i=0; i<arr2.length-1; i++) { //번지로 계산하는거니까 길이에서 -1을 해야함
+        	   temp[i] = arr2[i];
+        	   arr2[i] = arr2[i+2];
+        	   arr2[i+2] = arr2[i+1];
+        	   arr2[i+1] = temp[i];
+        	   if(i==0) {
+        		   for(i=0; i<arr2.length; i++) {
+                	   for(int j=0; j<arr2[i].length; j++) {
+                		   System.out.print(arr2[i][j]+" "); 
+                	   }
+                	   System.out.println();
+                   }
+        	   }
+           }
+           
+           
+        }
+		
+		
+        	
+
+		
+		
+		
+		
+		
 }
--- java_project/FirstJava/src/first/ArrayTest.java
+++ java_project/FirstJava/src/first/ArrayTest.java
@@ -78,62 +78,62 @@ public class ArrayTest {
 	
 	
 	
-	// 로또 번호 추출
-	// 정수 1~45까지 숫자를 저장하는 변수 45개 생성 -> int 타입의 배열 선언, 생성
-	int[] ball = new int[45]; 
+//	// 로또 번호 추출
+//	// 정수 1~45까지 숫자를 저장하는 변수 45개 생성 -> int 타입의 배열 선언, 생성
+//	int[] ball = new int[45]; 
+//	
+//	// 각 변수에 1~45까지 숫자 중복안되게 저장
+//	for(int i=0; i<ball.length; i++) { // i->0~44 index로 사용
+//		ball[i]=i+1;	
+//	}
+//	for(int i=0; i<ball.length; i++) {
+//		System.out.println(ball[i]);
+//	}
+//	// 각 index의 값들을 섞는 처리 : index=0의 값과 임의의(1~44) index의 값을 서로 바꾼다.
+//	
+//	int j=0;       //임의의 index
+//	int temp=0;    //각 변수의 값을 바꾸기 위한 임시변수
+//	
+//	for(int i=0; i<100; i++) {
+//		//임의의 index를 구한다 : 1~44
+//		j=(int)(Math.random()*44)+1;
+//		temp=ball[0];
+//		ball[0]=ball[j];
+//		ball[j]=temp;
+//	}
+//	
+//	// 배열에서 0~5까지의 변수값을 출력
+//	for(int i=0; i<6; i++) {
+//		System.out.print(ball[i]+"\t");
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
 	
-	// 각 변수에 1~45까지 숫자 중복안되게 저장
-	for(int i=0; i<ball.length; i++) { // i->0~44 index로 사용
-		ball[i]=i+1;	
-	}
-	for(int i=0; i<ball.length; i++) {
-		System.out.println(ball[i]);
-	}
-	// 각 index의 값들을 섞는 처리 : index=0의 값과 임의의(1~44) index의 값을 서로 바꾼다.
 	
-	int j=0;       //임의의 index
-	int temp=0;    //각 변수의 값을 바꾸기 위한 임시변수
-	
-	for(int i=0; i<100; i++) {
-		//임의의 index를 구한다 : 1~44
-		j=(int)(Math.random()*44)+1;
-		temp=ball[0];
-		ball[0]=ball[j];
-		ball[j]=temp;
-	}
-	
-	// 배열에서 0~5까지의 변수값을 출력
-	for(int i=0; i<6; i++) {
-		System.out.print(ball[i]+"\t");
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
 	// int타입의 배열을 받고 , 증가시킬 정수값을 받아서  
 	// 각 배열의 요소에 받은 정수값만큼 증가시키고 
 	// 전달받은 배열을 반환
-	static int[] addAllArray(int[] ar, int addValue) {     // 스태틱으로 만들어야함. static main에서 호출해야하기 때문에
-        // int[] ar = 0x100, int addValut = 7
-		for(int i=0; i<ar.length; i++) {
-			ar[i]=ar[i]+addValue;
-		}
-		
-		return ar; // 0x100의 값을 가짐.
+//	static int[] addAllArray(int[] ar, int addValue) {     // 스태틱으로 만들어야함. static main에서 호출해야하기 때문에
+//        // int[] ar = 0x100, int addVale = 7
+//		for(int i=0; i<ar.length; i++) {
+//			ar[i]=ar[i]+addValue;
+//		}
+//		
+//		return ar; // 0x100의 값을 가짐.
+//	
+//    }
 	
 	
 	
 	
-	}
-	*/
 	
 
 	
