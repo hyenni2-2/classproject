ccb68aad8b0e6dbb9d143c99888923ed3da549c3
HyeinChoi <hi.choi1031@gmail.com>
2020-11-14 15:45:04 +0900

    201114 Study

--- SQL/20201113_HW.sql
+++ SQL/20201113_HW.sql
@@ -40,7 +40,7 @@ where custid not in(select o.custid from orders o, customer c where o.custid = c
 
 -- (9) 주문금액의총액과
 -- 주문의평균금액
-select sum(saleprice), avg(saleprice)
+select sum(saleprice) as "TOTAL", avg(saleprice)
 from orders
 ;
 
@@ -88,8 +88,6 @@ from orders o, book b
 where o.bookid=b.bookid 
 and (b.price-o.saleprice)=(select max(b.price-o.saleprice) from orders o, book b where o.bookid=b.bookid);
 
-
-
 select * 
       from orders o, book b 
       where o.bookid=b.bookid 
@@ -98,10 +96,63 @@ select *
 -- (13) 도서의판매액평균보다
 -- 자신의구매액평균이더높은고객의이름
 select * from orders;
+select * from customer;
 
-select avg(saleprice)
+select avg(saleprice)  -- 도서 판매액 평균
+from orders;
+
+select custid,avg(saleprice) -- 자신의 구매액 평균
 from orders
+group by custid;
+
+select c.name
+from orders o, customer c
+where o.custid=c.custid
+group by c.name
+having avg(saleprice)>(select avg(saleprice) from orders)
 ;
+
+
+-- (1) 박지성이 구매한 도서의 출판사와 
+-- 같은 출판사에서 도서를 구매한 고객의 이름
+select * from book;
+select * from customer;
+select * from orders;
+
+-- 박지성이 구매한 도서의 출판사
+select c.name  
+from book b, orders o, customer c
+where b.bookid=o.bookid and 
+o.custid=c.custid and 
+b.publisher in
+(
+select b. publisher 
+from book b, orders o 
+where b.bookid=o.bookid and o.custid=1) and o.custid!=1
+;
+
+
+-- (2) 두 개 이상의 서로 다른 출판사에서 
+-- 도서를 구매한 고객의 이름
+select * from book;
+select * from customer;
+select * from orders;
+
+-- 구매한 고객 이름과 출판사 이름
+select c.name
+from orders o, customer c,book b
+where o.custid=c.custid and b.bookid=o.bookid 
+having count(distinct b.publisher)>=2
+group by c.name
+;
+
+select o.custid,b.publisher
+from orders o, book b
+where o.bookid=b.bookid
+order by o.custid
+;
+
+
 
 
 
