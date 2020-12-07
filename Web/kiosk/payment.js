7ee42f178069db9eeb36361bf13b46801f46907e
hyenni2-2 <hi.choi1031@gmail.com>
2020-12-07 18:32:12 +0900

    201207 PJT

---
+++ Web/kiosk/payment.js
@@ -0,0 +1,22 @@
+// 선택한 정보 저장된 것 (임시)
+var pay = [{
+        tabn: 1,
+        foodn: 2,
+        cnt: 1
+},
+    {
+        tabn: 3,
+        2,
+        foodn: 3,
+        3,
+        fnt: 2,
+        2
+}
+];
+
+
+// 결제 메소드 만들기 : each 메서드 사용 - 
+$.each(pay, function (index, item){
+    console.log(index,item); 
+    
+}); 
