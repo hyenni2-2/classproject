edbe9276602a521d77a4512756485bc5b2eba8f7
HyeinChoi <hi.choi1031@gmail.com>
2021-02-01 16:02:20 +0900

    210201 Spring Project

--- spring_project/MWeather/mwCloset/src/main/java/com/mw/closet/domain/Closet.java
+++ spring_project/MWeather/mwCloset/src/main/java/com/mw/closet/domain/Closet.java
@@ -2,29 +2,22 @@ package com.mw.closet.domain;
 
 import java.sql.Timestamp;
 
-import lombok.Data;
 
 // tbl_closet DB 정보
+public class Closet {
+	
+	private int cIdx;
+	private int memIdx;
+	private String cName;
+	private String cDPhoto;
+	private String cPhotoOut;
+	private String cMerDb;
+	private String cText;
+	private int cLike;
+	private int cBmk;
+	private Timestamp cDate;
+	private String cIp;
 
-public @Data class Closet {
 	
-	private int mem_idx;
-	private String c_name;
-	private String c_Dphoto;
-	private String c_PhotoOut;
-	private String c_MerDb;
-	private String c_Text;
-	private int c_Like;
-	private int c_Bmk;
-	private Timestamp c_Date;
-	private String c_Ip;
-	
-	// 페이징을 위한 생성자
-	public Closet(int mem_idx, String c_name, String c_PhotoOut, int c_Like, int c_Bmk) {
-		this.mem_idx = mem_idx;
-		this.c_name = c_name;
-		this.c_PhotoOut = c_PhotoOut;
-		this.c_Like = c_Like;
-		this.c_Bmk = c_Bmk;
-	}
+
 }
--- spring_project/MWeather/mwCloset/src/test/java/com/mw/closet/controller/ClosetListControllerTest.java
+++ spring_project/MWeather/mwCloset/src/test/java/com/mw/closet/controller/ClosetListControllerTest.java
@@ -2,14 +2,20 @@ package com.mw.closet.controller;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,14 +23,20 @@ import lombok.extern.log4j.Log4j;
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
 @Log4j
 public class ClosetListControllerTest {
+	
+	@Setter(onMethod_ = {@Autowired} )
+	private WebApplicationContext ctx;
+	
+	private MockMvc mvc;
 
 	@Before
 	public void setUp() throws Exception {
+		this.mvc = MockMvcBuilders.webAppContextSetup(ctx).build();
 	}
 
 	@Test
-	public void testGetClosetList() {
-		fail("Not yet implemented");
+	public void testClosetList() throws Exception {
+		log.info(mvc.perform(MockMvcRequestBuilders.get("/list")).andReturn().getModelAndView().getModelMap());
 	}
 
 }
